e895b225430d0a5242ccd9648e55024402a0658e
sayaka-ando <sayayann82@yahoo.co.jp>
2021-01-14 15:10:30 +0900

    テストコードの準備

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user do
+    password = Faker::Internet.password(min_length: 6)
+    email                  { Faker::Internet.free_email }
+    password               { password }
+    password_confirmation  { password }
+    name                   { '田中' }
+    employee_number        { '1234' }
+    
+  end
+end
