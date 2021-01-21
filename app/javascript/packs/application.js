0e59fb5066a04c7f9a93e91efd538113ff72ed1d
sayaka-ando <sayayann82@yahoo.co.jp>
2021-01-21 15:52:27 +0900

    gemのインストール

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,10 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require("chartkick")
+require("chart.js")
+require("chart.min")
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
